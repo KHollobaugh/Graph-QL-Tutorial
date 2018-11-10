c8c7b287ae8393d92dff1ba94b1755a56d0a236d
KHollobaugh <khollobaugh@gmail.com>
2018-11-10 16:38:20 -0500

    added newBook form

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+
+//allow cross-origin requests
+app.use(cors());
 
 mongoose.connect('mongodb://katy:test1234@ds157493.mlab.com:57493/gql-tutorial-kh', { useNewUrlParser: true })
 mongoose.connection.once('open', () => {
